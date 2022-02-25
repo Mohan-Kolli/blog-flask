08d4be9a58b68409bff9d80c8a5b92d86b6a4d53
Mohan Kolli <mohankolli@laptop.com>
2022-02-25 15:30:08 -0500

    integrate with git

---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class UserForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = StringField(label="Password", validators=[DataRequired()])
+    name = StringField(label="Name", validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
+
+class LoginUser(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
+
+class CommentForm(FlaskForm):
+    comment = CKEditorField(label="Comment", validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
