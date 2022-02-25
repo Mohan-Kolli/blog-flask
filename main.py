7e10514079f9d468f2d445908eb1b7cc3a9b602f
Mohan Kolli <mohankolli@laptop.com>
2022-02-25 16:30:34 -0500

    adding env variables

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -18,7 +20,7 @@ from forms import CreatePostForm, UserForm, LoginUser, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("AppKey")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
